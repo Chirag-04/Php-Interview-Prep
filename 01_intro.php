df914fff4eba3f1d38e532c9ae6c9283a8536ee1
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-06-12 12:12:38 +0530

    initial commt

---
+++ 01_intro.php
@@ -0,0 +1,25 @@
+
+<?php
+echo "First Tut";
+//comment in php
+//_________________________________
+// echo prints (takes multiple argument)
+// echo 1 ,"Hello World" , 123456; 
+
+//_________________________________
+// print - work like echo but takes single values 
+
+// print 123;
+//_________________________________
+// print_r() useful for arrays
+// print_r([1,2,3,5])
+
+//_________________________________
+//var_dump() return more info
+// var_dump([1,2,3,4,5]);
+
+//_________________________________
+//var_export() output a string
+// var_export(["Chirag maini"]); // array of string 
+//  var_export([1,2,3,4]);
+?>
